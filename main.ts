24f303169ee09521c3caa69fe3a0797c72a542d7
Ksungwon09 <129531909+Ksungwon09@users.noreply.github.com>
2023-05-22 14:58:40 +0900

    버그수정

--- main.ts
+++ main.ts
@@ -1,73 +1,45 @@
+// 우회전4
 input.onGesture(Gesture.TiltRight, function () {
     radio.sendNumber(3)
     basic.showLeds(`
         . . # . .
-        . # # . .
-        # . # # #
+        . . . # .
+        # # # # #
+        . . . # .
         . . # . .
-        # . # . .
         `)
 })
+// 좌회전5
 input.onGesture(Gesture.TiltLeft, function () {
     radio.sendNumber(4)
     basic.showLeds(`
-        . . # # #
         . . # . .
-        . . # # #
-        . . . . #
-        # . # # #
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
         `)
 })
-input.onGesture(Gesture.ScreenUp, function () {
-    if (input.buttonIsPressed(Button.AB)) {
-        radio.sendNumber(2)
-        basic.showLeds(`
-            . . # # #
-            . . . . #
-            . . # # #
-            . . # . #
-            # . # # #
-            `)
-    }
-    if (input.buttonIsPressed(Button.A)) {
-        radio.sendNumber(5)
-        basic.showLeds(`
-            . . # . .
-            . . # . .
-            . . # # #
-            . . # . #
-            # . # # #
-            `)
-    }
-    if (input.buttonIsPressed(Button.B)) {
-        radio.sendNumber(6)
-        basic.showLeds(`
-            . . # # #
-            . . # . #
-            . . . . #
-            . . . . #
-            # . . . #
-            `)
-    }
-})
+// 후진2
 input.onGesture(Gesture.LogoDown, function () {
     radio.sendNumber(1)
     basic.showLeds(`
-        . . # # #
-        . . . . #
-        . . # # #
         . . # . .
-        # . # # #
+        . . # . .
+        # . # . #
+        . # # # .
+        . . # . .
         `)
 })
+// 전진1
 input.onGesture(Gesture.LogoUp, function () {
     radio.sendNumber(0)
     basic.showLeds(`
         . . # . .
+        . # # # .
+        # . # . #
         . . # . .
         . . # . .
-        . . # . .
-        # . # . .
         `)
 })
 radio.setGroup(122)
@@ -79,5 +51,37 @@ basic.showLeds(`
     . . . . .
     `)
 basic.forever(function () {
-	
+    if (input.isGesture(Gesture.ScreenUp)) {
+        // 고우6
+        if (input.buttonIsPressed(Button.A)) {
+            radio.sendNumber(5)
+            basic.showLeds(`
+                . . # . .
+                . . # . .
+                . . # # #
+                . . # . #
+                # . # # #
+                `)
+        }
+        // 고좌7
+        if (input.buttonIsPressed(Button.B)) {
+            radio.sendNumber(6)
+            basic.showLeds(`
+                . . # # #
+                . . # . #
+                . . . . #
+                . . . . #
+                # . . . #
+                `)
+        }
+        // 종지3
+        radio.sendNumber(2)
+        basic.showLeds(`
+            . . # # #
+            . . . . #
+            . . # # #
+            . . . . #
+            # . # # #
+            `)
+    }
 })
